bcda08ec4a01922b630bede38571b158a1e68d87
Amandada666 <amanda.gerard19@hotmail.fr>
2025-12-02 11:14:09 +0100

    cv

--- src/components/PDF/PDFDownloadButton.tsx
+++ src/components/PDF/PDFDownloadButton.tsx
@@ -9,7 +9,7 @@ interface PDFDownloadButtonProps {
 const PDFDownloadButton: React.FC<PDFDownloadButtonProps> = ({ secret }) => {
   return (
     <ButtonLink
-      href='/CV-RJ.pdf'
+      href='/cv-amanda-gerard-2025.pdf'
       // href={secret ? `/api/pdf?secret=${secret}` : '/api/pdf'}
       size="lg"
       target="_blank"
